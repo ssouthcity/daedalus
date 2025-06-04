5784578393f41f6e0049d3a20159fce7164771e8
ssouthcity <51554341+ssouthcity@users.noreply.github.com>
2025-06-05 00:10:35 +0200

    refactor: decouple ldtk entity spawning (#8)

--- src/player.rs
+++ src/player.rs
@@ -1,8 +1,5 @@
 use avian2d::prelude::*;
 use bevy::prelude::*;
-use bevy_ecs_ldtk::prelude::*;
-
-use crate::camera::CameraTarget;
 
 const PLAYER_WALK_SPEED: f32 = 96.0;
 const PLAYER_RUN_SPEED: f32 = 128.0;
@@ -11,38 +8,12 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.register_ldtk_entity::<PlayerEntity>("Player")
-            .add_systems(Update, process_player)
-            .add_systems(Update, move_player);
+        app.add_systems(Update, move_player);
     }
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug, Default, Component)]
 pub struct Player;
-
-#[derive(Clone, Default, Bundle, LdtkEntity)]
-pub struct PlayerEntity {
-    pub player: Player,
-    #[sprite_sheet]
-    pub sprite_sheet: Sprite,
-    pub camera_target: CameraTarget,
-}
-
-pub fn process_player(mut commands: Commands, entity_query: Query<Entity, Added<Player>>) {
-    for entity in entity_query.iter() {
-        commands.entity(entity).insert((
-            RigidBody::Dynamic,
-            LockedAxes::ROTATION_LOCKED,
-            LinearVelocity::ZERO,
-        ));
-
-        commands.spawn((
-            ChildOf(entity),
-            Transform::from_xyz(0.0, -8.0, 0.0),
-            Collider::rectangle(4.0, 4.0),
-        ));
-    }
-}
 
 pub fn move_player(
     mut query: Query<&mut LinearVelocity, With<Player>>,
