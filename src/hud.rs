0a18ae1afcdf2567cf05064b6d4daef38cab8b13
ssouthcity <51554341+ssouthcity@users.noreply.github.com>
2025-06-06 20:10:13 +0200

    feat: add hud and healing (#9)

---
+++ src/hud.rs
@@ -0,0 +1,35 @@
+use bevy::prelude::*;
+
+use crate::{field, player};
+
+pub struct HudPlugin;
+
+impl Plugin for HudPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup_hud_system)
+            .add_systems(Update, update_hp_system);
+    }
+}
+
+#[derive(Component)]
+struct HudHPText;
+
+fn setup_hud_system(mut commands: Commands) {
+    commands
+        .spawn(Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            padding: UiRect::axes(Val::Px(32.0), Val::Px(24.0)),
+            ..default()
+        })
+        .with_children(|commands| {
+            commands.spawn((HudHPText, Text::default()));
+        });
+}
+
+fn update_hp_system(
+    mut hud_hp_text: Single<&mut Text, With<HudHPText>>,
+    hp: Single<&field::Health, With<player::Player>>,
+) {
+    hud_hp_text.0 = hp.0.to_string();
+}
