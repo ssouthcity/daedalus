99ffdd952a262b56ae28cf4befddf263df6b0c7f
ssouthcity <51554341+ssouthcity@users.noreply.github.com>
2025-06-19 22:15:28 +0200

    feat: add audio transitions (#14)

--- src/audio.rs
+++ src/audio.rs
@@ -1,38 +1,78 @@
 use bevy::{audio::Volume, input::common_conditions::input_just_pressed, prelude::*};
 
 pub(super) fn plugin(app: &mut App) {
-    app.register_type::<Music>();
+    app.register_type::<BackgroundMusic>();
     app.register_type::<SoundEffect>();
     app.register_type::<AudioSettings>();
+    app.register_type::<FadeAudio>();
+
+    app.register_type::<MasterVolume>();
+
+    app.add_event::<TransitionBackgroundMusic>();
 
     app.init_resource::<AudioSettings>();
-
-    app.add_systems(Startup, setup_background_music);
+    app.insert_resource(MasterVolume(0.5));
 
     app.add_systems(
         Update,
         (
-            apply_global_volume.run_if(resource_changed::<GlobalVolume>),
-            apply_audio_settings.run_if(resource_changed::<AudioSettings>),
+            fade_music,
+            transition_background_music,
             // temporary solution until settings menu
             mute_audio.run_if(input_just_pressed(KeyCode::KeyM)),
+            increase_master_volume.run_if(input_just_pressed(KeyCode::Period)),
+            decrease_master_volume.run_if(input_just_pressed(KeyCode::Comma)),
         ),
     );
+
+    app.add_systems(PostUpdate, apply_relative_volume);
+}
+
+#[derive(Resource, Reflect)]
+#[reflect(Resource)]
+pub struct MasterVolume(f32);
+
+#[derive(Component, Reflect)]
+#[reflect(Component)]
+pub struct RelativeVolume(f32);
+
+impl Default for RelativeVolume {
+    fn default() -> Self {
+        Self(1.0)
+    }
+}
+
+impl RelativeVolume {
+    const SILENT: RelativeVolume = RelativeVolume(0.0);
+}
+
+fn apply_relative_volume(
+    master_volume: Res<MasterVolume>,
+    master_mute: Res<AudioSettings>,
+    query: Query<(&mut AudioSink, &RelativeVolume)>,
+) {
+    for (mut sink, relative_volume) in query {
+        let composed_volume = master_volume.0 * relative_volume.0;
+
+        let volume = if master_mute.muted() {
+            0.0
+        } else {
+            composed_volume
+        };
+
+        sink.set_volume(Volume::Linear(volume));
+    }
 }
 
 #[derive(Resource, Reflect)]
 #[reflect(Resource)]
 pub struct AudioSettings {
     is_muted: bool,
-    global_volume: f32,
 }
 
 impl Default for AudioSettings {
     fn default() -> Self {
-        Self {
-            is_muted: false,
-            global_volume: 0.5,
-        }
+        Self { is_muted: false }
     }
 }
 
@@ -48,17 +88,68 @@ impl AudioSettings {
 
 #[derive(Component, Reflect)]
 #[reflect(Component)]
-pub struct Music;
+pub struct BackgroundMusic;
 
-fn setup_background_music(asset_server: Res<AssetServer>, mut commands: Commands) {
-    let audio = asset_server.load("music/background_music.ogg");
+#[derive(Component, Reflect, PartialEq, Eq)]
+#[reflect(Component)]
+pub enum FadeAudio {
+    Out,
+    In,
+}
 
-    commands.spawn((
+const FADE_TIME_SECS: f32 = 2.0;
+
+fn fade_music(
+    mut commands: Commands,
+    query: Query<(Entity, &mut RelativeVolume, &FadeAudio)>,
+    time: Res<Time>,
+) {
+    for (entity, mut volume, fade) in query {
+        let delta = time.delta_secs() / FADE_TIME_SECS;
+
+        let new_volume = match fade {
+            FadeAudio::In => (volume.0 + delta).min(1.0),
+            FadeAudio::Out => (volume.0 - delta).max(0.0),
+        };
+
+        if *fade == FadeAudio::Out && new_volume <= 0.0 {
+            commands.entity(entity).despawn();
+        }
+
+        volume.0 = new_volume;
+    }
+}
+
+fn background_music(audio: Handle<AudioSource>) -> impl Bundle {
+    (
         Name::new("Background Music"),
-        Music,
+        BackgroundMusic,
         AudioPlayer::new(audio),
-        PlaybackSettings::LOOP,
-    ));
+        PlaybackSettings::LOOP.with_volume(Volume::SILENT),
+        FadeAudio::In,
+        RelativeVolume::SILENT,
+    )
+}
+
+#[derive(Event)]
+pub struct TransitionBackgroundMusic(pub Handle<AudioSource>);
+
+fn transition_background_music(
+    mut commands: Commands,
+    query: Query<Entity, With<BackgroundMusic>>,
+    mut events: EventReader<TransitionBackgroundMusic>,
+) {
+    if events.is_empty() {
+        return;
+    }
+
+    for music in query {
+        commands.entity(music).insert(FadeAudio::Out);
+    }
+
+    for event in events.read() {
+        commands.spawn(background_music(event.0.clone()));
+    }
 }
 
 #[derive(Component, Reflect)]
@@ -70,31 +161,18 @@ pub fn sound_effect(audio: Handle<AudioSource>) -> impl Bundle {
         SoundEffect,
         AudioPlayer::new(audio),
         PlaybackSettings::DESPAWN,
+        RelativeVolume::default(),
     )
-}
-
-fn apply_global_volume(
-    global_volume: Res<GlobalVolume>,
-    mut audio_query: Query<(&PlaybackSettings, &mut AudioSink)>,
-) {
-    for (playback, mut sink) in &mut audio_query {
-        sink.set_volume(global_volume.volume * playback.volume);
-    }
 }
 
 fn mute_audio(mut audio_settings: ResMut<AudioSettings>) {
     audio_settings.toggle_mute();
 }
 
-fn apply_audio_settings(
-    audio_settings: Res<AudioSettings>,
-    mut global_volume: ResMut<GlobalVolume>,
-) {
-    let vol = if audio_settings.muted() {
-        0.0
-    } else {
-        audio_settings.global_volume
-    };
+fn increase_master_volume(mut master_volume: ResMut<MasterVolume>) {
+    master_volume.0 = (master_volume.0 + 0.1).min(1.0);
+}
 
-    global_volume.volume = Volume::Linear(vol);
+fn decrease_master_volume(mut master_volume: ResMut<MasterVolume>) {
+    master_volume.0 = (master_volume.0 - 0.1).max(0.0);
 }
